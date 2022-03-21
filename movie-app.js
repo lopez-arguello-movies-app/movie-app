6ba20e935c1c575680f13681437602f1140e67ac
Samuel Arguello <sam.rene12@gmail.com>
2022-03-21 16:34:26 -0500

    began work on post functionality

--- movie-app.js
+++ movie-app.js
@@ -1,15 +1,15 @@
 "use strict";
 
 function getMovies(){
-    fetch("movies.json").then(resp => resp.json()).then(data => $("#movie-container").html(createMovieCards(data)));
+    fetch("movies.json").then(response => response.json()).then(data => $("#movie-container").html(createMovieCards(data)));
 }
 
-getMovies();
+getMovies()
 
 function createMovieCards(data) {
     let html = '';
 // creates 5 duplicate cards with different data populated by the API
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < data.movies.length; i++) {
 // takes in date data in the API and displays it in a more readable format
         html += `<div class="movie-cards">
                             <h5>${data.movies[i].title}</h5>
@@ -19,4 +19,24 @@ function createMovieCards(data) {
     }
     return html;
 }
+
+const addedMovies = {
+        title: $("#title").val(),
+        rating: $("#rating").val(),
+        id: 5
+}
+
+console.log(addedMovies);
+
+const postOption = {
+    method: "POST",
+    headers: {
+        "Content-Type" : "application/json"
+    },
+    body: JSON.stringify(addedMovies)
+};
+
+$("button").click(function(){
+    console.log("hello");
+})
 
